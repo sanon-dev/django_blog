c380c79b8790e8d2cde797b8caa191b7a53b2295
AJ0629 <47250909+AJ0629@users.noreply.github.com>
2021-08-17 13:08:36 -0400

    Working with databases via ORM. Added post class to views and now using as context instead of dummy data. Minor HTML changes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone 
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)      #characters field with max length rescription of 100
+    content = models.TextField()
+    #date_posted = models.DateTimeField(auto_now_add= True) #auto_now_add = True sets the time to when first posted, but cant ever update
+    date_posted = models.DateTimeField(default = timezone.now)
+    last_modified = models.DateTimeField(auto_now= True) #every time post is changed this will be set the time it was sent
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #if user is deleted, delete the post as well
+
+    def __str__(self):
+        return self.title
